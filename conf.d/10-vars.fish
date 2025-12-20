da94a9a813e6567f82b6a4dc8cc1498e6fb97639
Andrew Voeyko <andrew.voeicko@yandex.ru>
2025-12-20 23:19:39 +0300

    FIX CHECKING FOR EXECUTABLE

--- conf.d/10-vars.fish
+++ conf.d/10-vars.fish
@@ -9,17 +9,19 @@ if status is-interactive
     # set $SSH_AUTH_SOCK
     set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 
-    if type go &>/dev/null && set -q GOPATH && test -d $GOPATH/bin
+    if type -q go && set -q GOPATH && test -d $GOPATH/bin
         fish_add_path --path $GOPATH/bin
     end
 
-    if type cargo &>/dev/null && set -q CARGO_PATH && test -d $CARGO_PATH/bin
-        fish_add_path --path $CARGO_PATH/bin
+    if type -q cargo && set -q CARGO_HOME && test -d $CARGO_HOME/bin
+        fish_add_path --path $CARGO_HOME/bin
     end
 
-    if type hx &>/dev/null
+    if type -q hx
         fish_add_path --path $HOME/.local/share/helix/LSP/bin
-        # set $EDITOR
         set -x EDITOR hx
+    else if type -q helix
+        fish_add_path --path $HOME/.local/share/helix/LSP/bin
+        set -x EDITOR helix
     end
 end
