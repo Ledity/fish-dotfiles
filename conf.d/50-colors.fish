8f0d3280a87b00ee46ecd77f4e721b2acae7a966
Andrew Voeyko <andrew.voeicko@yandex.ru>
2026-01-10 17:38:52 +0300

    REMOVE ITALIC

--- conf.d/50-colors.fish
+++ conf.d/50-colors.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     set fish_color_command blue --bold
-    set fish_color_quote green --bold --italics
+    set fish_color_quote green --bold
     set fish_color_error red --bold
     set fish_color_option yellow
     set fish_color_comment yellow --bold --dim
